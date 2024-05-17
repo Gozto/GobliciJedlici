c6a9731927fdbde6700f5b7eed15eca9fc0f175e
Adam <122052237+Gozto@users.noreply.github.com>
2024-05-17 12:05:59 +0200

    Delete goblici jedlici/gobliciJedlici directory

--- goblici jedlici/gobliciJedlici/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.goblicijedlici {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.goblicijedlici to javafx.fxml;
-    exports com.example.goblicijedlici;
-}
